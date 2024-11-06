264ac3a34ad4b68331077811715d55857b374b59
Willian Silva <willian.silva@evobiolab.com>
2024-11-06 11:01:35 +0100

    Update biomaRt-annotations-genes-gal7b.R

--- biomaRt-annotations-genes-gal7b.R
+++ biomaRt-annotations-genes-gal7b.R
@@ -20,13 +20,13 @@ sink(paste0(OUTPUTLOCATION,"/job",JOBID,".Rlog"),type=c("output","message"))
 #Input $1: Job ID.
 #Input $2: Output location.
 #Input $3: Output file tag.
-#Input $4: Chromosome (or comma-separated vector of chromosome names).
-#Input $5: Start position (or comma-separated vector of start positions).
-#Input $6: End position (or comma-separated vector of end positions).
+#Input $4: Chromosome (or comma-separated list of chromosome names).
+#Input $5: Start position (or comma-separated list of start positions).
+#Input $6: End position (or comma-separated list of end positions).
 #Output: File with annotations (*.txt).
 
 #Usage: 
-#Rscript --vanilla template-Rscript.R <JOB ID> <OUTPUT LOCATION> <FILE TAG> <CHROMOSOME> <START POSITION> <END POSITIONS>
+#Rscript --vanilla biomaRt-annotations-genes-gal7b.R <JOB ID> <OUTPUT LOCATION> <FILE TAG> <CHROMOSOME> <START POSITION> <END POSITIONS>
 
 ############################################################################
 
@@ -92,49 +92,12 @@ INPUTDATA$Chromosome<-gsub("Chr","",INPUTDATA$Chromosome) #Remove "Chr" from chr
 INPUTDATA$Chromosome<-gsub("chr","",INPUTDATA$Chromosome) #Remove "chr" from chromosome names.
 INPUTDATA
 
-#List available types of annotation.
-#listEnsembl() 
-#ensembl_genes<-useEnsembl(biomart="genes") #Genes.
-#ensembl_snps<-useEnsembl(biomart="snps") #SNPs.
-
-#List available datasets.
-#listDatasets(ensembl_genes)
-#searchDatasets(mart=ensembl_genes,pattern="gallus") #Genes.
-#listDatasets(ensembl_snps)
-#searchDatasets(mart=ensembl_snps,pattern="gallus") #SNPs.
-
-#Update Mart object with desired dataset.
+#Load Mart object with desired dataset.
 ensembl_genes_gal7b<-useEnsembl(biomart="genes",dataset="ggallus_gene_ensembl") #Genes.
-#ensembl_snps_gal7b<-useEnsembl(biomart="snps",dataset="ggallus_snp") #SNPs.
 
-#List filters and attributes
-#listFilters(ensembl_genes_gal7b) #List filters.
-#listAttributes(ensembl_genes_gal7b) #List attributes.
-#listFilters(ensembl_snps_gal7b) #List filters.
-#listAttributes(ensembl_snps_gal7b) #List attributes.
-
-#Search for specific filters and attributes.
-#searchFilters(mart=ensembl_genes_gal7b,pattern=".*chromosome.*")
-#searchFilters(mart=ensembl_genes_gal7b,pattern=".*start.*")
-#searchFilters(mart=ensembl_genes_gal7b,pattern=".*end.*")
-#searchAttributes(mart=ensembl_genes_gal7b,pattern=".*end.*")
-#searchAttributes(mart=ensembl_genes_gal7b,pattern=".*id.*")
-#searchAttributes(mart=ensembl_genes_gal7b,pattern=".*name.*")
-#listAttributes(ensembl_genes_gal7b,page="feature_page")
-#listAttributes(ensembl_genes_gal7b,page="sequences")
-#listAttributes(ensembl_snps_gal7b,page="snp")
-
-#List filter options.
-#listFilterOptions(mart=ensembl_genes_gal7b,filter="chromosome_name") 
-#listFilterOptions(mart=ensembl_snps_gal7b,filter="chr_name") 
-
-#Query gene database.
+#Query database.
 DATA_GENES<-getBM(attributes=c("chromosome_name","start_position","end_position","strand",
                                "ensembl_gene_id","external_gene_name","gene_biotype",
-                               #"ensembl_transcript_id","external_transcript_name",
-                               #"transcript_start","transcript_end",
-                               #"transcript_length","transcript_biotype",
-                               #"ensembl_peptide_id","protein_id",
                                "description"),
                   filters=c("chromosome_name","start","end"),
                   values=list("chromosome_name"=INPUTDATA$Chromosome,
@@ -142,35 +105,8 @@ DATA_GENES<-getBM(attributes=c("chromosome_name","start_position","end_position"
                               "end"=INPUTDATA$End),
                   mart=ensembl_genes_gal7b)
 
-#Query gene database.
-#DATA_TRANSCRIPTS<-getBM(attributes=c("chromosome_name","start_position","end_position","strand",
-#                               #"ensembl_gene_id","external_gene_name","gene_biotype",
-#                               "ensembl_transcript_id","external_transcript_name",
-#                               "transcript_start","transcript_end",
-#                               "transcript_length","transcript_biotype",
-#                               #"ensembl_peptide_id","protein_id",
-#                               "description"),
-#                  filters=c("chromosome_name","start","end"),
-#                  values=list("chromosome_name"=INPUTDATA$Chromosome,
-#                              "start"=INPUTDATA$Start,
-#                              "end"=INPUTDATA$End),
-#                  mart=ensembl_genes_gal7b)
-
-#Query SNPs database.
-#DATA_SNPS<-getBM(attributes=c("chr_name","chrom_start","chrom_strand","refsnp_id",
-#                              "allele","allele_1","ensembl_gene_stable_id","ensembl_type"),
-#                 filters=c("chr_name","start","end"),
-#                 values=list("chr_name"=INPUTDATA$Chromosome,
-#                             "start"=INPUTDATA$Start,
-#                             "end"=INPUTDATA$End),
-#                 mart=ensembl_snps_gal7b)
-#DATA_SNPS
-
 #Save formatted data.
 write.table(DATA_GENES,file=OUTPUTFILE,sep="\t",row.names=FALSE,col.names=TRUE)
-#write.csv(DATA_GENES,file=OUTPUTFILE,row.names=FALSE)
-#write.csv(DATA_TRANSCRIPTS,file=OUTPUTFILE,row.names=FALSE)
-#write.csv(DATA_SNPS,file=OUTPUTFILE,row.names=FALSE)
 
 ############################################################################
 ############################################################################
