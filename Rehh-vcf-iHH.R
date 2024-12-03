1388220b227d806f14405262242fbc622f904387
Willian Silva <willian.silva@evobiolab.com>
2024-12-03 22:36:27 +0100

    Update Rehh-vcf-iHH.R

--- Rehh-vcf-iHH.R
+++ Rehh-vcf-iHH.R
@@ -91,7 +91,7 @@ cat(paste0("PROCESSING FILE: ",INPUTFILE,"\n"))
 cat("Converting data into an object of class haplohh (rehh::data2haplohh).\n")
 HAPLODATA<-data2haplohh(hap_file=INPUTFILE,
                         polarize_vcf=FALSE, #Unpolarized data.
-                        min_maf=0.05, #Filter data on a minor allele frequency or MAF.
+                        min_maf=0.0, #Filter data on a minor allele frequency or MAF.
                         vcf_reader="data.table",
                         verbose=TRUE)
 #saveRDS(HAPLODATA,file=paste0(OUTPUTFILEPREFIX,".haplohh-job",JOBID,".rds"))
@@ -100,8 +100,7 @@ cat("Computing EHH based statistics over a whole chromosome (rehh::scan_hh).\n")
 #Calculate iHH statistics.
 HAPLODATA_iHH<-scan_hh(HAPLODATA,
                         phased=PHASINGSTATUS, #Phased data?
-                        polarized=FALSE, #Unpolarized data.
-                        threads=20) 
+                        polarized=FALSE) #Unpolarized data.
 
 #Sleep.
 Sys.sleep(5)
